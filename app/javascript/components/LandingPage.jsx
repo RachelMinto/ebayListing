b69bae407acecee1586d822af6dba845b0344900
Rachel Minto <rminto@wistia.com>
2018-01-01 14:12:37 -0500

    Home page styles.

--- app/javascript/components/LandingPage.jsx
+++ app/javascript/components/LandingPage.jsx
@@ -4,12 +4,18 @@ export default class LandingPage extends React.Component {
   render() {
     return (
       <div className="page">
-        <div className="landing-page padded--lg">
+        <div className="landing-page padded--lg container--contents-centered">
           <h1 className="text--header landing-page--title">
             Clausmology
           </h1>
           <hr></hr>
-          <div className="centered">Randomized characters and log lines</div>
+          <div className="padded--sm">Randomized characters and log lines
+          </div>
+          <div className="padded--lg container">
+            <div className="container--opaque">
+              Create a free account to make your own.
+            </div>
+          </div>
         </div>
       </div>
     );
