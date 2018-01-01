ba4709fe9435c945db176bd789dcf15cc89f6830
Rachel Minto <rminto@wistia.com>
2018-01-01 10:24:06 -0500

    Set up sass files.

--- app/javascript/components/SampleComponent.jsx
+++ app/javascript/components/SampleComponent.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default class SampleComponent extends React.Component {
   render() {
     return (
-    <h1>
-      Hi, I'm SampleComponent!
+    <h1 className="text--header">
+      Clausmology
     </h1>
   );
   }
