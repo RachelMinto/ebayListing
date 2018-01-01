6e46dcf72bd900be3937a9e24dec00eddb9579ba
Rachel Minto <rminto@wistia.com>
2018-01-01 13:31:51 -0500

    Login button styles.

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -4,9 +4,9 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <div className="container--filled container--padded">
-          <div className="login">
-            <button>Login!</button>
+        <div className="container container--filled padded--sm header--container">
+          <div className="login float--right">
+            <button className="padded--sm btn">Login</button>
           </div>
         </div>
       </div>
--- app/javascript/components/LandingPage.jsx
+++ app/javascript/components/LandingPage.jsx
@@ -4,10 +4,12 @@ export default class LandingPage extends React.Component {
   render() {
     return (
       <div className="page">
-        <div className="landing-page">
+        <div className="landing-page padded--lg">
           <h1 className="text--header landing-page--title">
             Clausmology
           </h1>
+          <hr></hr>
+          <div className="centered">Randomized characters and log lines</div>
         </div>
       </div>
     );
